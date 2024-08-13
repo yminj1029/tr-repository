c2dc0e78a6ca46ff36ecbc0aad4609b3e4c2527e
yminj1029 <minjiyun204@gmail.com>
2024-08-13 21:19:56 +0900

    jotai dev tool 오류 해결

--- config/jotai-provider.tsx
+++ config/jotai-provider.tsx
@@ -1,21 +1,28 @@
 'use client';
 
 import { Provider, createStore } from 'jotai';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import { DevTools } from 'jotai-devtools';
 import 'jotai-devtools/styles.css';
 
 const customStore = createStore();
 
-// children의 타입을 ReactNode로 지정합니다.
 interface ProvidersProps {
 	children: ReactNode;
 }
 
 export const JotaiProvider = ({ children }: ProvidersProps) => {
+	const [isClient, setIsClient] = useState(false);
+
+	useEffect(() => {
+		if (process.env.NODE_ENV === 'development') {
+			setIsClient(true);
+		}
+	}, []);
+
 	return (
 		<Provider store={customStore}>
-			<DevTools store={customStore} />
+			{isClient && <DevTools />}
 			{children}
 		</Provider>
 	);
