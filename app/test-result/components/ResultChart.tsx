cff83ac28359181c282c9b054e28ea856419cd2e
yminj1029 <minjiyun204@gmail.com>
2024-09-05 01:07:40 +0900

    레이더 차트 완료

--- app/test-result/components/ResultChart.tsx
+++ app/test-result/components/ResultChart.tsx
@@ -1,36 +1,19 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Group } from '@visx/group';
 import { scaleLinear } from '@visx/scale';
 import { Point } from '@visx/point';
 import { Line, LineRadial } from '@visx/shape';
 import { Text } from '@visx/text'; // 텍스트 추가를 위해 import
 import { curveLinearClosed } from 'd3-shape'; // 극좌표계일 경우 마지막 결과를 닫음
+import { Test, testAtom } from '@/atom/testAtom';
+import { useAtom } from 'jotai';
 
-interface chartData {
-	category: string,
-	score: number
-}
 const blue = '#3399ff';
 export const bluePoint = '#3333ff';
 const silver = '#e6e6e6';
-// export const background = '#FFFFF';
 export const background = '#FFFFFF';
 
 const degrees = Math.PI * 2;
-
-//실제 데이터 값(나중에 atom에 있는 값을 꺼내와야함)
-const data = [
-	{ category: '9 화합', score: 13 },
-	{ category: '1 개혁', score: 12 },
-	{ category: '2 협력', score: 32 },
-	{ category: '3 성취', score: 22 },
-	{ category: '4 창조', score: 19 },
-	{ category: '5 탐구', score: 33 },
-	{ category: '6 헌신', score: 22 },
-	{ category: '7 열정', score: 12 },
-	{ category: '8 도전', score: 10 },
-];
-
 const legend = [
 	{ category: '9 화합', type: "TYPE_9" },
 	{ category: '1 개혁', type: "TYPE_1" },
@@ -43,17 +26,21 @@ const legend = [
 	{ category: '8 도전', type: "TYPE_8" },
 ]
 
-
 //타입은 9가지
 const numberOfTypes = 9;
 
 // 레이더 선 그려질 때 계층
 const levels = 9;
 
+type AngleData = {
+	angle: number;
+};
 // 레이더 차트를 그리기 위한 일정한 각도를 생성
-const genWebsAngles = Array.from({ length: numberOfTypes }, (_, i) => ({
+const genWebsAngles: AngleData[] = Array.from({ length: numberOfTypes }, (_, i) => ({
 	angle: i * (degrees / numberOfTypes)
 }));
+
+const getAngle = (d: AngleData) => d.angle;
 
 // 각 레이더 차트의 꼭지점(x, y) 생성
 const genWebsPoints = (angle: number, radius: number) => {
@@ -63,7 +50,32 @@ const genWebsPoints = (angle: number, radius: number) => {
 	}
 };
 
+// 데이터를 이용한 폴리곤 그리기
+const genPolygonPoints = (testData: Test, angles: AngleData[], scale: (n: number) => number, getAngleData: (d: AngleData) => number) => {
+	const testDataList: { key: string, value: number }[] = Object.entries(testData).map(([key, value]) => ({ key: key, value: value }))
+	const DataListWithKeys = [testDataList[testDataList.length - 1], ...testDataList.slice(0, -1)];
 
+	const points: { x: number; y: number; value: number }[] = new Array(DataListWithKeys.length).fill({
+		x: 0,
+		y: 0,
+		value: 0
+	});
+
+	const pointString: string = new Array(DataListWithKeys.length)
+		.fill('')
+		.reduce((acc, _, i) => {
+			// if (i > DataListWithKeys.length) return acc;
+			const { value } = DataListWithKeys[i]
+			const angle = getAngleData(angles[i])
+			const xVal = scale(value) * Math.cos(angle - Math.PI / 2)
+			const yVal = scale(value) * Math.sin(angle - Math.PI / 2)
+			points[i] = { x: xVal, y: yVal, value };
+			acc += `${xVal},${yVal} `;
+			return acc;
+		}, '')
+
+	return { points, pointString }
+}
 // 차트의 기본 마진
 const defaultMargin = { top: 0, left: 80, right: 80, bottom: 80 };
 
@@ -86,6 +98,19 @@ export default function TestResult({
 
 	// 레이더 차트에서 (x1,y1)
 	const zeroPoint = new Point({ x: 0, y: 0 });
+
+	// scaleLineaer는 입력값을 선형적으로 변환하는 스케일 함수
+	// 레이더 차트에서 그려지는 폴리곤 최대 값은 45로 지정 : 45를 기준으로 반지름 길이를 반환.
+	const yScale = scaleLinear<number>({
+		range: [0, radius],
+		domain: [0, 45],
+	});
+
+	// atom에 보관한 데이터 가져오기
+	const [test] = useAtom(testAtom);
+
+	// 화면에 그릴 데이터, 각도 리스트, 최댓값 계산 함수, angle값 가져오는 함수
+	const polygonPoints = genPolygonPoints(test, genWebsAngles, d => yScale(d) ?? 0, getAngle)
 
 	return width < 10 ? null : (
 		<svg width={width} height={height}>
@@ -122,19 +147,44 @@ export default function TestResult({
 						key={`category-label-${i}`}
 						x={genWebsPoints(angle, radius + 16).x}
 						y={genWebsPoints(angle, radius + 16).y}
-						fontSize={14}
+						fontSize={12}
 						fill={blue}
 						textAnchor="middle"
 					>
 						{legend[i].category}
 					</Text>
 				))}
-
-
-
-
-
 				{/* 레이더 차트 webs 영역 end */}
+				{/* 데이터 폴리곤 영역 start */}
+				<polygon
+					points={polygonPoints.pointString}
+					fill={blue}
+					fillOpacity={0.3}
+					stroke={blue}
+					strokeWidth={1}
+				/>
+				{polygonPoints.points.map((point, i) => (
+					<circle
+						key={`radar-point-${i}`}
+						cx={point.x}
+						cy={point.y}
+						r={4}
+						fill={bluePoint}
+					/>
+				))}
+				{polygonPoints.points.map((point, i) => (
+					<Text
+						key={`point-label-${i}`}
+						x={point.x + 8}
+						y={point.y - 4}
+						fontSize={14}
+						fill={bluePoint}
+						textAnchor="start"
+					>
+						{point.value}
+					</Text>
+				))}
+				{/* 데이터 폴리곤 영역 ends */}
 			</Group>
 		</svg>
 	);
